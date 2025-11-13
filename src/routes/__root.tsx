d70301c4a63b38efedd1566d8e54a530e5d5d1e9
Cameron Roberts <cameronroberts1998@gmail.com>
2025-11-13 12:16:05 -0800

    added styling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import NavBar from '../components/NavBar'
+import '../styles/default.css'
 
 const RootLayout = () => (
   <>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
 
 import { createFileRoute } from '@tanstack/react-router'
+import GridView from '../components/gridview'
+import { projectList } from '../assets/data/projects'
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -7,8 +9,11 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="steve">
-      <h3>Welcome Home!</h3>
+    <><div className="home">
+      <h1>Cameron Roberts</h1>
+      <h6>Developer Portfolio</h6>
     </div>
+      <GridView items={projectList}></GridView>
+    </>
   )
 }
