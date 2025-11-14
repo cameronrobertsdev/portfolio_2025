b9570a60c153ee1f0d0e7cfe2f9c4e646e8c2a20
Cameron Roberts <cameronroberts1998@gmail.com>
2025-11-13 19:15:08 -0800

    updated base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/portfolio_2025/',
   plugins: [
 
      tanstackRouter({
